8d5f104d96a4069be708f286d2373b785527660c
Kevin Davies <kswkev@hotmail.com>
2022-05-30 10:57:01 +0100

    Exercise6EqualSumChecker added

---
+++ Section 4 Methods and more/Exercise6EqualSumChecker/src/academy/learnprogramming/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int value1, int value2, int expectedSum) {
+        return (value1 + value2) == expectedSum;
+    }
+}
