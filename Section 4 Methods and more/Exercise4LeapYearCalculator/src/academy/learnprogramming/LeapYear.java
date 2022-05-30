7cbcef4d2e842fb90bbeb71702d07f88a9a8a1ba
Kevin Davies <kswkev@hotmail.com>
2022-05-30 10:28:54 +0100

    Exercise4LeapYearCalculator added

---
+++ Section 4 Methods and more/Exercise4LeapYearCalculator/src/academy/learnprogramming/LeapYear.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+
+        if (year < 1 || year > 9999){
+            return false;
+        }
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                return true;
+            } else {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+}
