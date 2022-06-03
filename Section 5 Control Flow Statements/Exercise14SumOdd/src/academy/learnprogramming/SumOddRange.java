6a86c3d0addf95c7894d5f20b0a7f430428df972
Kevin Davies <kswkev@hotmail.com>
2022-06-03 11:40:35 +0100

    Exercise14SumOdd added

---
+++ Section 5 Control Flow Statements/Exercise14SumOdd/src/academy/learnprogramming/SumOddRange.java
@@ -0,0 +1,30 @@
+package academy.learnprogramming;
+
+public class SumOddRange {
+
+    public static int sumOdd(int start, int end) {
+
+        if (start > end || start <= 0 || end <= 0) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        for (int i =  start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    public static boolean isOdd(int number) {
+
+        if (number < 0) {
+            return false;
+        }
+
+        return (number % 2 != 0);
+    }
+}
