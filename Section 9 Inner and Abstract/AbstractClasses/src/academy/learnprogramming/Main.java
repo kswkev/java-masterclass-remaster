69a2b5beb376d00682a553c318edfb90b6cf793b
Kevin Davies <kswkev@hotmail.com>
2022-07-08 12:46:05 +0100

    AbstractClasses added

---
+++ Section 9 Inner and Abstract/AbstractClasses/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Ben");
+
+        dog.eat();
+        dog.breathe();
+
+    }
+}
