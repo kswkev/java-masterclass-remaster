f9fbc8916cd68b33f5e64715389c531d627210c9
Kevin Davies <kswkev@hotmail.com>
2022-07-14 14:22:49 +0100

    PackagesChallenge added

--- Section 11 Naming Conventions and Packages/PackagesChallengeSeries/src/com/kswkev/com/kswkev/Math/Series.java
+++ Section 11 Naming Conventions and Packages/PackagesChallengeSeries/src/com/kswkev/com/kswkev/Math/Series.java
@@ -39,4 +39,15 @@ public class Series {
         }
         return current;
     }
+
+//    nicer way of doing fibonacci with recurrent calls
+    public static int fib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib(n-1) + fib(n-2);
+        }
+    }
 }
